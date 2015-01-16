13630dfc9f8ef6c2f1e4ca3fb8b38b93dbbb1790
RakshithaG <rakshithagw@gmail.com>
2015-01-16 13:11:32 +0530

    Added a button to hide notifications

--- PlatformProject.SPA/App/app/app.js
+++ PlatformProject.SPA/App/app/app.js
@@ -58,41 +58,51 @@ var notifyApp = angular.module('notifyApp', []);
 notifyApp.controller("notifyController", function ($scope) {
 
     $scope.show = function (data, type, position) {
-        setInterval(function () {
-            var para = document.createElement("p");
-            var node = document.createTextNode(data);
-            para.appendChild(node);
-            var element = document.getElementById("notify");
-            element.appendChild(para);
+      setInterval(function () {
+          //if (document.getElementById("notify").style.visibility == 'hidden') {
+             // document.getElementById("notify").style.visibility='visible';
+          //}
+        
+          //else{
+              var para = document.createElement("p");
+              var node = document.createTextNode(data);
+              para.appendChild(node);
+              var element = document.getElementById("notify");
+              element.appendChild(para);
 
-            var style = document.createElement('style');
-            style.type = 'text/css';
-            style.innerHTML = '.cssClass { color: white; height: 20px;width:250px; padding:20px; text-align:center';
-            document.getElementsByTagName('head')[0].appendChild(style);
-            document.getElementById('notify').className = 'cssClass';
-            if (type == 'success') {
-                element.style.backgroundColor = '#00CC00';
-            }
-            else if (type == 'error') {
-                element.style.backgroundColor = '#E62E19';
-            }
-            else if (type == 'info') {
-                element.style.backgroundColor = '#2368FF';
-            }
-            if (position == 'right') {
-                element.style.marginLeft = '922px';
-            }
-            else if (position == 'left') {
-                element.style.marginLeft = '0px';
-            }
-            else if (position == 'center') {
-                element.style.marginLeft = '500px';
-            }
-
-
-        }, 2000);
-
+              var style = document.createElement('style');
+              style.type = 'text/css';
+              style.innerHTML = '.cssClass { color: white; height: 20px;width:250px; padding:20px; text-align:center';
+              document.getElementsByTagName('head')[0].appendChild(style);
+              document.getElementById('notify').className = 'cssClass';
+              if (type == 'success') {
+                  element.style.backgroundColor = '#00CC00';
+              }
+              else if (type == 'error') {
+                  element.style.backgroundColor = '#E62E19';
+              }
+              else if (type == 'info') {
+                  element.style.backgroundColor = '#2368FF';
+              }
+              if (position == 'right') {
+                  element.style.marginLeft = '922px';
+              }
+              else if (position == 'left') {
+                  element.style.marginLeft = '0px';
+              }
+              else if (position == 'center') {
+                  element.style.marginLeft = '500px';
+              }
+         // }
+    }, 1000);
+   
     };
+
+    $scope.hide = function () {
+        //document.getElementById("notify").style.visibility='hidden';
+        document.getElementById("notify").style.display = 'none';
+    };
+
     $scope.alert = function () {
         alert('alerted!');
     };
