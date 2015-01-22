b6e1bcb151274188e1803d0dd2a6a8fb5cc57fd8
Niranjan Kumar <niranjanku@outlook.com>
2015-01-22 16:08:08 +0530

    Integrating the Multitenancy with STS

---
+++ Demos/SaasKit.Demos.Nancy/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SaasKit.Demos.Nancy
+{
+    using Microsoft.Owin.Hosting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startOptions = new StartOptions();
+            //startOptions.Urls.Add("http://localhost:8000");
+            //startOptions.Urls.Add("http://dev.local:8000");
+
+            startOptions.Urls.Add("http://localhost:9000");
+            startOptions.Urls.Add("http://127.0.0.1:9000");
+            
+            using (WebApp.Start<Startup>(startOptions))
+            {
+                Console.WriteLine("Running on {0}", string.Join(" and ", startOptions.Urls.ToArray()));
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
