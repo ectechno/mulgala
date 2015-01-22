77c3cadbc5dd3646161de47846cc1faf4924f962
Lasantha Bandara <lasanthab@outlook.com>
2015-01-22 13:02:40 +0530

    finished solution structure in web project

--- PlatformProject.SPA/App/devoops.js
+++ PlatformProject.SPA/App/devoops.js
@@ -3357,7 +3357,7 @@ $(document).ready(function () {
 	});
 	var ajax_url = location.hash.replace(/^#/, '');
 	if (ajax_url.length < 1) {
-	    ajax_url = '~/App/app/dashboard/dashboard.html';
+	    ajax_url = '~/App/dashboard/dashboard.html';
 	}
 //	LoadAjaxContent(ajax_url);
 	var item = $('.main-menu li a[href$="' + ajax_url + '"]');
