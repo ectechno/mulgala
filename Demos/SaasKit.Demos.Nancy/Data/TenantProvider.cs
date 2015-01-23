555f40980998db5ac6c3f6d3a68f4e486a5309c3
titus nanda kumara <titus.nandakumara@gmail.com>
2015-01-23 14:07:24 +0530

    tenants can add using an array

---
+++ Demos/SaasKit.Demos.Nancy/Data/TenantProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasKit.Demos.Nancy.Data
+{
+    public class TenantProvider:ITenantIdentifier
+    {
+        
+
+        public UserTenant GetTenant(string host)
+        {
+            UserTenant[] tenants = SaasKit.Demos.Nancy.Program.TenantList;
+
+            int i;
+            for (i = 0; i < tenants.Length; i++)
+            {
+                if (tenants[i].HostName == host)
+                {
+                    return tenants[i];
+                }
+            }
+            return null;
+
+        }
+    }
+}
--- Demos/SaasKit.Demos.Nancy/Program.cs
+++ Demos/SaasKit.Demos.Nancy/Program.cs
@@ -1,12 +1,17 @@
-﻿namespace SaasKit.Demos.Nancy
+﻿using SaasKit.Demos.Nancy.Data;
+
+namespace SaasKit.Demos.Nancy
 {
     using Microsoft.Owin.Hosting;
     using System;
     using System.Collections.Generic;
     using System.Linq;
+   
 
-    class Program
+    public class Program
     {
+        public static UserTenant[] TenantList;
+
         static void Main(string[] args)
         {
             var startOptions = new StartOptions();
@@ -14,14 +19,31 @@
             //startOptions.Urls.Add("http://dev.local:8000");
 
             //In the host file, add these two hostNames
-            startOptions.Urls.Add("http://t1.localhost:9000");
-            startOptions.Urls.Add("http://t2.localhost:9000");
-            
+            //startOptions.Urls.Add("http://t1.localhost:9000");
+            //startOptions.Urls.Add("http://t2.localhost:9000");
+
+
+            UserTenant t1 = new UserTenant("Tenant1", 0, "t1.localhost", "127.0.0.1", 9000);
+            UserTenant t2 = new UserTenant("Tenant2", 1, "t2.localhost", "127.0.0.1", 9000);
+
+            TenantList = new[] {t1, t2};
+
+            RegisterAllTenants(TenantList, startOptions);
+
+
             using (WebApp.Start<Startup>(startOptions))
             {
                 Console.WriteLine("Running on {0}", string.Join(" and ", startOptions.Urls.ToArray()));
                 Console.WriteLine("Press enter to exit");
                 Console.ReadLine();
+            }
+        }
+
+        private static void RegisterAllTenants(UserTenant[] TenantList, StartOptions startOptions)
+        {
+            foreach (UserTenant t in TenantList)
+            {
+                startOptions.Urls.Add("http://"+t.HostName+":"+t.Port);
             }
         }
     }
