513ff18b3028ff6205a5d28b26187a7d5f3e57df
RakshithaG <rakshithagw@gmail.com>
2015-01-29 15:16:52 +0530

    Changed the folder structure of admin console

--- PlatformProject.SPA/App/AT/app/app.js
+++ PlatformProject.SPA/App/AT/app/app.js
@@ -4,36 +4,57 @@ app.config(function ($routeProvider) {
     $routeProvider
         .when('/Dashboard', {
             //controller: 'Controller',
-            templateUrl: 'app/dashboard.html'
+            templateUrl: 'app/Views/dashboard.html'
         })
         .when('/Create', {
             //controller: 'Controller',
-            templateUrl: 'app/create.html'
+            templateUrl: 'app/Views/create.html'
         })
          .when('/Widgets', {
              //controller: 'Controller',
-             templateUrl: 'app/widgets.html'
+             templateUrl: 'app/Views/widgets.html'
          })
         .when('/Licensing', {
             //controller: 'Controller',
-            templateUrl: 'app/licensing.html'
+            templateUrl: 'app/Views/licensing.html'
         })
         .when('/Statistics', {
             //controller: 'Controller',
-            templateUrl: 'app/statistics.html'
+            templateUrl: 'app/Views/statistics.html'
         })
          .when('/Create/CreateTenant', {
              //controller: 'Controller',
-             templateUrl: 'app/createTenant.html'
+             templateUrl: 'app/Views/createTenant.html'
          })
         .when('/Create/CreateUser', {
             //controller: 'Controller',
-            templateUrl: 'app/createUser.html'
+            templateUrl: 'app/Views/createUser.html'
         })
          .when('/Create/CreateTenant/CreateAdmin', {
              //controller: 'Controller',
-             templateUrl: 'app/createAdmin.html'
+             templateUrl: 'app/Views/createAdmin.html'
          })
        
         .otherwise({ redirectTo: '/' });
+});
+
+app.controller('submission', function ($scope, $http, $location) {
+    $scope.save = function () {
+        $http({
+            method: "POST",
+            url: "Bills_Angular",
+            data: {
+                Fornecedor: $scope.fornecedor
+            },
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }).success(function (data) {
+            //to change the angular view use $location t0 redirect
+            $location.path("/route-where-you-want-to-go");
+            //to redirect to page outside current angular app use 'window.location':
+            //window.location = "/path-where-you-want-to-go
+
+        }).error(function () {
+            alert("Error");
+        });
+    };
 });
