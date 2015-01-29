7eccae35dfd66bb25863fc0d9a100731a053c954
RakshithaG <rakshithagw@gmail.com>
2015-01-29 13:16:18 +0530

    Added basic admin console panel

---
+++ PlatformProject.SPA/App/AT/app/app.js
@@ -0,0 +1,30 @@
+﻿var app = angular.module('admin', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/Dashboard', {
+            //controller: 'Controller',
+            templateUrl: 'app/dashboard.html'
+        })
+        .when('/Create', {
+            //controller: 'Controller',
+            templateUrl: 'app/create.html'
+        })
+         .when('/Widgets', {
+             //controller: 'Controller',
+             templateUrl: 'app/widgets.html'
+         })
+        .when('/Licensing', {
+            //controller: 'Controller',
+            templateUrl: 'app/licensing.html'
+        })
+        .when('/Statistics', {
+            //controller: 'Controller',
+            templateUrl: 'app/statistics.html'
+        })
+         .when('/Create/CreateTenant', {
+             //controller: 'Controller',
+             templateUrl: 'app/createTenant.html'
+         })
+        .otherwise({ redirectTo: '/' });
+});
