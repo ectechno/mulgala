963622d777747f62b6f7a4e3eb0d3d653dcefec1
Lukshica <lukshica@gmail.com>
2015-02-11 12:07:36 +0530

    bower component for material design

---
+++ PlatformProject.AdminConsole/app/Services/SharedServices.js
@@ -0,0 +1,9 @@
+﻿//Created by Madushanka on 29/01/2015.
+
+angular.module('admin').service('SharedServices', ['$window', function ($window)
+{
+    function locateToWindow(path)
+    {
+        $window.location.href = path;
+    }
+}]);
---
+++ PlatformProject.SPA/bower_components/justified-gallery/Gruntfile.js
@@ -0,0 +1,131 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('justifiedGallery.jquery.json'),
+    banner: '/*!\n' +
+      ' * <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n' +
+      '<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
+      ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+      ' * Licensed under the <%= _.pluck(pkg.licenses, "type").join(", ") %> license.\n' + 
+      ' */\n',
+    // Task configuration.
+    clean: {
+      files: ['dist']
+    },
+
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        files: {
+          'dist/js/jquery.<%= pkg.name %>.js': ['src/js/<%= pkg.name %>.js'],
+          'dist/css/<%= pkg.name %>.css': ['dist/css/<%= pkg.name %>.css'],
+          'dist/css/<%= pkg.name %>.min.css': ['dist/css/<%= pkg.name %>.min.css']
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: ['dist/js/jquery.<%= pkg.name %>.js'],
+        dest: 'dist/js/jquery.<%= pkg.name %>.min.js'
+      }
+    },
+
+    jshint: {
+      src: {
+        options: {
+          jshintrc: 'src/.jshintrc'
+        },
+        src: ['src/js/*.js']
+      }
+    },
+
+    less: {
+      development: {
+        options: {
+          paths: ["src/less"]
+        },
+        files: {
+          'dist/css/<%= pkg.name %>.css': ['src/less/<%= pkg.name %>.less']
+        }
+      },
+      production: {
+        options: {
+          cleancss: true,
+          report: 'min'
+        },
+        files: {
+          'dist/css/<%= pkg.name %>.min.css': ['dist/css/<%= pkg.name %>.css']
+        }
+      }
+    },
+
+    csslint: {
+      options: {
+        csslintrc: 'src/less/.csslintrc'
+      },
+      src: [
+        'dist/css/*.css'
+      ]
+    },
+
+    compress: {
+      main: {
+        options: {
+          archive: 'dist/<%= pkg.name %>.zip'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'dist/css/',
+            src: ['<%= pkg.name %>.css'],
+            dest: '/'
+          },
+          {
+            expand: true,
+            cwd: 'dist/js/',
+            src: ['jquery.<%= pkg.name %>.js'],
+            dest: '/'
+          }
+        ]
+      },
+      minified: {
+        options: {
+          archive: 'dist/<%= pkg.name %>.min.zip'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'dist/css/',
+            src: ['<%= pkg.name %>.min.css'],
+            dest: '/'
+          },
+          {
+            expand: true,
+            cwd: 'dist/js/',
+            src: ['jquery.<%= pkg.name %>.min.js'],
+            dest: '/'
+          }
+        ]
+      }
+    }
+
+  });
+
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt);
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'less', 'csslint', 'concat', 'uglify', 'compress']);
+
+};
