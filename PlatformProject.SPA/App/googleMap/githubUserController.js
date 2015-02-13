ef4a6a2788f5db27b2407d684f8e6982c2f9c59b
Lukshica <lukshica@gmail.com>
2015-02-13 07:43:55 +0530

    Sample Service

---
+++ PlatformProject.SPA/App/googleMap/githubUserController.js
@@ -0,0 +1,15 @@
+﻿app.controller('ServiceController', ['$scope', 'githubService',
+    function ($scope, githubService) {
+        // Watch for changes on the username property.
+        // If there is a change, run the function
+        $scope.$watch('username', function (newUsername) {
+            // uses the $http service to call the GitHub API
+            // and returns the resulting promise
+            githubService.events(newUsername)
+              .success(function (data, status, headers) {
+                  // the success function wraps the response in data
+                  // so we need to call data.data to fetch the raw data
+                  $scope.events = data.data;
+              })
+        });
+    }]);
