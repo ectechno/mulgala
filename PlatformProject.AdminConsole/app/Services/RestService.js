02eec1d44518b59948cc6f11ae5a2ff68c51fb71
nalin madushanka <nalink@99x.lk>
2015-02-24 12:44:15 +0530

    add functions

--- PlatformProject.AdminConsole/app/Services/RestService.js
+++ PlatformProject.AdminConsole/app/Services/RestService.js
@@ -6,4 +6,21 @@ angular.module('admin').factory('RestService', function ($resource)
     {
         return $resource(restPath); 
     };
+
+    this.get = function (restPath,getParam)
+    {
+        return this.requestData(restPath).get(getParam, function () { });
+    }
+    this.getAll = function (restPath, getParam)
+    {
+        return this.requestData(restPath).query(getParam, function () { });
+    }
+    this.save = function (restPath, data)
+    {
+        this.requestData(restPath).save(data, function () { });
+    }
+    this.update = function (restPath, data)
+    {
+        this.requestData(restPath).update(data, function () { });
+    }
 })
