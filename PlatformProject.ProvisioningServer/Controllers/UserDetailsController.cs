a5f620028a8ea177f20e04cb90d83ea1a9366ffb
titus nanda kumara <titus.nandakumara@gmail.com>
2015-03-06 12:14:47 +0530

    controller for userdetails

---
+++ PlatformProject.ProvisioningServer/Controllers/UserDetailsController.cs
@@ -0,0 +1,51 @@
+﻿using PlatformProject.Data;
+using PlatformProject.DTO;
+using PlatformProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PlatformProject.ProvisioningServer.Controllers
+{
+    public class UserDetailsController : ApiController
+    {
+        private UnitOfWork unitOfWork;
+        private IRepository<User> userRepository;
+        private IRepository<Tenant> tenantRepository;
+        private IRepository<Role> roleRepository;
+
+        protected UserDetailsController()
+        {
+            unitOfWork = new UnitOfWork();
+            userRepository = unitOfWork.GetRepository<User>();
+            tenantRepository = unitOfWork.GetRepository<Tenant>();
+            roleRepository = unitOfWork.GetRepository<Role>();
+        }
+
+        // GET api/userDetails/5
+        [Route("~/api/userdetails/{tenantString}/{userString}")]
+        public HttpResponseMessage Get(string tenantString,string userString)
+        {
+            
+
+            HttpResponseMessage response;
+            
+
+            response = Request.CreateResponse(HttpStatusCode.OK, new UserDTO
+            {
+
+                Name = userString,
+
+                Tenant = tenantString
+            });
+            return response;
+
+
+
+        }
+
+    }
+}
